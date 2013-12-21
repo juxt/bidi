a18130f407a6c5ecd11b99f8c015e95045821fb3
Malcolm Sparks <malcolm@juxt.pro>
2013-12-21 00:35:52 +0000

    Preparing for a 1.0.0 release

--- project.clj
+++ project.clj
@@ -1,6 +1,17 @@
-(defproject bidi "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+;; Copyright © 2013, JUXT LTD. All Rights Reserved.
+;;
+;; The use and distribution terms for this software are covered by the
+;; Eclipse Public License 1.0 (http://opensource.org/licenses/eclipse-1.0.php)
+;; which can be found in the file epl-v10.html at the root of this distribution.
+;;
+;; By using this software in any fashion, you are agreeing to be bound by the
+;; terms of this license.
+;;
+;; You must not remove this notice, or any other, from this software.
+
+(defproject bidi "1.0.0"
+  :description "Bidirectional URI routing"
+  :url "https://github.com/juxt/bidi"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]])
