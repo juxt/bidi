e3b8754a5a11e5dc2378c04a2c247f3b22f04b5b
Malcolm Sparks <malcolm@juxt.pro>
2014-02-23 13:22:06 +0000

    Fixing version for minor upgrade

--- project.clj
+++ project.clj
@@ -9,7 +9,7 @@
 ;;
 ;; You must not remove this notice, or any other, from this software.
 
-(defproject bidi "1.9.5-SNAPSHOT"
+(defproject bidi "1.10.0-SNAPSHOT"
   :description "Bidirectional URI routing"
   :url "https://github.com/juxt/bidi"
   :license {:name "Eclipse Public License"
