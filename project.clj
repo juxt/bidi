d4bbd2ad353b8936af27c8f3058a792a9412ef58
Malcolm Sparks <malcolm@juxt.pro>
2014-02-23 13:24:46 +0000

    lein-release plugin: preparing 1.10.0 release

--- project.clj
+++ project.clj
@@ -9,7 +9,7 @@
 ;;
 ;; You must not remove this notice, or any other, from this software.
 
-(defproject bidi "1.10.0-SNAPSHOT"
+(defproject bidi "1.10.0"
   :description "Bidirectional URI routing"
   :url "https://github.com/juxt/bidi"
   :license {:name "Eclipse Public License"
