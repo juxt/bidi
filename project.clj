25005fe10e6b339450ae870a75cdad28ca439094
Malcolm Sparks <malcolm@juxt.pro>
2014-12-01 12:59:42 +0000

    Test release 1.12.1

--- project.clj
+++ project.clj
@@ -9,7 +9,7 @@
 ;;
 ;; You must not remove this notice, or any other, from this software.
 
-(defproject bidi "1.12.1-SNAPSHOT"
+(defproject bidi "1.12.1"
   :description "Bidirectional URI routing"
   :url "https://github.com/juxt/bidi"
   :license {:name "Eclipse Public License"
