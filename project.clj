a201275efaeaaa53b9656662e938a0b1f4729c3e
Malcolm Sparks <malcolm@juxt.pro>
2014-12-22 18:05:04 +0000

    Move from EPL to MIT license

--- project.clj
+++ project.clj
@@ -1,19 +1,10 @@
-;; Copyright © 2013, JUXT LTD. All Rights Reserved.
-;;
-;; The use and distribution terms for this software are covered by the
-;; Eclipse Public License 1.0 (http://opensource.org/licenses/eclipse-1.0.php)
-;; which can be found in the file epl-v10.html at the root of this distribution.
-;;
-;; By using this software in any fashion, you are agreeing to be bound by the
-;; terms of this license.
-;;
-;; You must not remove this notice, or any other, from this software.
+;; Copyright © 2014, JUXT LTD.
 
-(defproject bidi "1.12.2"
+(defproject bidi "2.0.0-SNAPSHOT"
   :description "Bidirectional URI routing"
   :url "https://github.com/juxt/bidi"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "The MIT License"
+            :url "http://opensource.org/licenses/MIT"}
 
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/clojurescript "0.0-2371"]
