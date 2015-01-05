593b5e1b8cb664c5c38a8d460ce18e856a964dc1
Malcolm Sparks <malcolm@juxt.pro>
2015-01-05 17:10:34 +0000

    Use protocol for Ring handling

--- src/bidi/ring.clj
+++ src/bidi/ring.clj
@@ -4,6 +4,14 @@
   (:require
    [bidi.bidi :as bidi :refer :all]))
 
+(defprotocol Handle
+  (handle-request [_ req route-context]))
+
+(extend-protocol Handle
+  clojure.lang.Fn
+  (handle-request [f req _]
+    (f req)))
+
 (defn make-handler
   "Create a Ring handler from the route definition data
   structure. Matches a handler from the uri in the request, and invokes
@@ -12,10 +20,14 @@
       (assert route "Cannot create a Ring handler with a nil Route(s) parameter")
       (fn [{:keys [uri path-info] :as request}]
         (let [path (or path-info uri)
-              {:keys [handler route-params]} (apply match-route route path (apply concat (seq request)))]
+              {:keys [handler route-params] :as route-context}
+              (apply match-route route path (apply concat (seq request)))]
           (when handler
-            ((handler-fn handler)
+            (handle-request
+             (handler-fn handler)
              (-> request
                  (update-in [:params] merge route-params)
-                 (update-in [:route-params] merge route-params)))))))
+                 (update-in [:route-params] merge route-params))
+             route-context
+             )))))
    ([route] (make-handler route identity)))
