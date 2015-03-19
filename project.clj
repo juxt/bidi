c2f8e386678d75583c2bfd84bceb3a26696a78ab
Malcolm Sparks <malcolm@juxt.pro>
2015-03-19 21:41:24 +0000

    Ready for next release

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 ;; Copyright © 2014, JUXT LTD.
 
-(defproject bidi "1.18.8"
+(defproject bidi "1.18.9-SNAPSHOT"
   :description "Bidirectional URI routing"
   :url "https://github.com/juxt/bidi"
   :license {:name "The MIT License"
