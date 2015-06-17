93a31b3eafbc42554874325f01a8b053d312a0dd
Malcolm Sparks <malcolm@juxt.pro>
2015-06-17 14:06:18 +0100

    Upgrade ring-core

--- project.clj
+++ project.clj
@@ -10,7 +10,7 @@
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/clojurescript "0.0-2371"]
                  [com.cemerick/url "0.1.1"]
-                 [ring/ring-core "1.2.1"]]
+                 [ring/ring-core "1.3.2"]]
 
   :plugins [[lein-cljsbuild "1.0.3"]
             [com.cemerick/clojurescript.test "0.3.1"]]
