103f73be1fd0e10ba1c8515e4cba6971e5cc51cb
Nate Smith <nwjsmith@gmail.com>
2015-10-15 22:03:39 -0400

    Convert `bidi.bidi` to use reader conditionals
    Remove cljx now that everything is converted.

--- project.clj
+++ project.clj
@@ -14,33 +14,7 @@
   :plugins [[lein-cljsbuild "1.0.3"]
             [com.cemerick/clojurescript.test "0.3.1"]]
 
-  :prep-tasks [["cljx" "once"] "javac" "compile"]
-
-  :cljx {:builds [{:source-paths ["src"]
-                   :output-path "target/generated/src/clj"
-                   :rules :clj}
-                  {:source-paths ["src"]
-                   :output-path "target/generated/src/cljs"
-                   :rules :cljs}
-                  {:source-paths ["test"]
-                   :output-path "target/generated/test/clj"
-                   :rules :clj}
-                  {:source-paths ["test"]
-                   :output-path "target/generated/test/cljs"
-                   :rules :cljs}]}
-
-  :profiles {:dev {:dependencies [[org.clojure/clojure "1.7.0"]
-                                  [org.clojure/clojurescript "1.7.145"]
-                                  [ring/ring-mock "0.3.0"]
-                                  [compojure "1.4.0"]
-                                  [criterium "0.4.3"]]
-
-                   :plugins [[com.keminglabs/cljx "0.5.0"]]}}
-
-  :aliases {"deploy" ["do" "clean," "cljx" "once," "deploy" "clojars"]
-            "test" ["do" "clean," "cljx" "once," "test," "with-profile" "dev" "cljsbuild" "test"]}
-
-  :jar-exclusions [#"\.cljx|\.swp|\.swo|\.DS_Store"]
+  :prep-tasks ["javac" "compile"]
 
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.7.0"]
                                   [org.clojure/clojurescript "1.7.145"]
@@ -53,8 +27,11 @@
 
   :jar-exclusions [#"\.swp|\.swo|\.DS_Store"]
 
-  :lein-release {:deploy-via :shell
-                 :shell ["lein" "deploy"]}
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.7.0"]
+                                  [org.clojure/clojurescript "1.7.145"]
+                                  [ring/ring-mock "0.3.0"]
+                                  [compojure "1.4.0"]
+                                  [criterium "0.4.3"]]}}
 
   :cljsbuild {:test-commands {"unit" ["phantomjs" :runner
                                       "window.literal_js_was_evaluated=true"
